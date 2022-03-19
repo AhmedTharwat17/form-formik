647224c0cdb58d035273e461fc190546fec95c0c
AhmedTharwat17 <92866219+AhmedTharwat17@users.noreply.github.com>
2022-03-19 13:51:57 +0200

    first commit

---
+++ src/Components/regisiter/register.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import { TextField } from './TextField';
+import * as Yup from 'yup';
+
+import { useState} from "react"
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from "../../store/actions/auth";
+
+
+ const Register = () => {
+
+  const [successful, setSuccessful] = useState(false);
+  const { message } = useSelector(state => state.message);
+  const dispatch = useDispatch()
+  
+  const validate = Yup.object({
+      username: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .min(5, 'must be more than 5')
+      .required('Required'),
+    email: Yup.string()
+      .email('Email is invalid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(6, 'Password must be at least 6 charaters')
+      .required('Password is required'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Password must match')
+      .required('Confirm password is required'),
+  })
+  const handleRegister = (e) => {
+    setSuccessful(false);
+    console.log(e.username)
+    dispatch(register(e.username, e.email, e.password))
+    .then(() => {
+        setSuccessful(true);
+    })
+    .catch(() => {
+        setSuccessful(false);
+    });
+  };
+
+  return (
+  <>
+    <Formik
+      initialValues={{
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }}
+      validationSchema={validate}
+      onSubmit={handleRegister}
+    >
+      {formik => (
+        <div>
+          <h1 className="my-4 font-weight-bold .display-4">Sign Up</h1>
+          <Form>
+           <TextField label="User Name" name="username" type="text" />
+
+            <TextField label="Email" name="email" type="email" />
+            <TextField label="password" name="password" type="password" />
+            <TextField label="Confirm Password" name="confirmPassword" type="password" />
+            <button className="btn btn-success mt-3 me-3 btnn" type="submit" disabled={!formik.isValid}>Register</button>
+            <button className="btn btn-danger mt-3 " type="reset">Reset</button>
+          </Form>
+        </div>
+      )}
+    </Formik>
+
+    {message && (
+      <div className="form-group">
+        <div className={ successful ? "alert alert-success" : "alert alert-danger" } role="alert">
+          {message}
+        </div>
+      </div>
+    )}
+    </>
+  )
+}
+export default Register
